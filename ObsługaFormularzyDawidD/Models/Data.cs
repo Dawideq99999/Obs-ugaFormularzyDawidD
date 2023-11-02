02a718cff394b848eb3b3db9ddc3f4c7c164807c
HP <HP@LAPTOP-UC86IDGV>
2023-11-02 14:48:04 +0100

    Dodaj pliki projektów.

---
+++ ObsługaFormularzyDawidD/Models/Data.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObsługaFormularzyDawidD.Models
+{
+    public class Data
+    {
+        [Required(ErrorMessage = "Please fill your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please fill your e-mail address")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        
+        
+    }
+}
+
